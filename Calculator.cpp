861c18a1677e18bf6793aa85499c37d2aba6bd7e
malik charles <likiecharles@gmail.com>
2024-09-24 18:08:02 -0400

    fIRST COMMIT

---
+++ Calculator.cpp
@@ -0,0 +1,39 @@
+//Creates a calculator
+#include <iostream>
+using namespace std;
+
+int main() {
+double num1, num2; 
+char operation;
+
+//Prompt user for input
+cout << "Enter the first number: ";
+cin >> num1;
+
+cout << "Enter an operator (+, -, *, /)";
+cin >> operation;
+
+cout << "Enter the second number: ";
+cin >> num2;
+
+//Perform the calculation
+switch (operation)
+{
+case '+':
+    cout << "Result" << num1 + num2 << endl;
+    break; 
+case '-': 
+    cout << "Result" << num1 - num2 << endl;
+    break;
+case '*':
+ cout << "Result" << num1 * num2 << endl;
+ break;
+
+case '/':
+  cout << "Result" << num1 / num2 << endl;
+  break;
+
+}
+
+
+}
